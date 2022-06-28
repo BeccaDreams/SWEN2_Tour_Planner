f20c032063524cc04a4e6abe598788be9f125035
Fatema Aly <if20b057@technikum-wien.at>
2022-06-28 12:51:21 +0200

    implemented pdf reports generation

---
+++ Tour_Planner_BL/ReportController.cs
@@ -0,0 +1,31 @@
+﻿using Shared.Models;
+using Tour_Planner_DAL;
+
+namespace Tour_Planner_BL
+{
+    public class ReportController
+    {
+        ReportGenerator _reportGenerator;
+
+        public ReportController() 
+        { 
+            _reportGenerator = new ReportGenerator();
+        }
+
+        public void GenerateTourReport(Tour tour)
+        {
+            var tourLogDataHandler = new TourLogDataHandler();
+            var logs = tourLogDataHandler.getTourLogsByTourId(tour.Id);
+
+            _reportGenerator.GenerateTourReport(tour, logs);
+        }
+        
+        public void GenerateSummarizeReport()
+        {
+            var tourDataHandler = new TourDataHandler();
+            var tours = tourDataHandler.getTours();
+
+            _reportGenerator.GenerateSummarizeReport(tours);
+        }
+    }
+}

ef472097c5a42b649109badbf074044d13626bc3
Fatema Aly <if20b057@technikum-wien.at>
2022-06-28 14:20:28 +0200

    import/export implemeneted

---
+++ Tour_Planner_BL/JsonImporter.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tour_Planner_BL
+{
+    public class JsonImporter
+    {
+        public JsonImporter() 
+        {
+        }
+
+        public List<Tour> ImportTours(string filePath)
+        {
+            var tours = new List<Tour>();
+            using (StreamReader r = new StreamReader(filePath))
+            {
+                string json = r.ReadToEnd();
+                tours = JsonConvert.DeserializeObject<List<Tour>>(json);
+            }
+            return tours;
+        }
+    }
+}
--- Tour_Planner_BL/TourController.cs
+++ Tour_Planner_BL/TourController.cs
@@ -11,31 +11,54 @@ namespace Tour_Planner_BL
     public class TourController
     {
         private TourDataHandler handler;
+        private JsonExporter exporter;
+        private JsonImporter importer;
+
         public TourController()
         {
             handler = new TourDataHandler();
+            exporter = new JsonExporter();
+            importer = new JsonImporter();
         }
 
-        private List<Tour> Controller_getTours()
+        public List<Tour> Controller_getTours()
         {
             var TourList = handler.getTours();
             return TourList;
         }
 
-        private void Controller_addTour( Tour newTour )
+        public void Controller_addTour( Tour newTour )
         {
             handler.addTour(newTour);
         }
 
-        private void Controller_editTour(Tour tour)
+        public void Controller_editTour(Tour tour)
         {
             handler.updateTour(tour);
         }
 
-        private void Controller_deleteTour(/* Tour delTour */)
+        public void Controller_deleteTour(/* Tour delTour */)
         {
             //db.deleteTour(delTour);
         }
+
+        public void Controller_export()
+        {
+            var tours = Controller_getTours();
+            exporter.ExportTours(tours);
+        }
+
+        public void Controller_import()
+        {
+            var tours = importer.ImportTours("import/tours.json");
+
+            foreach (var tour in tours)
+            {
+                Controller_addTour(tour);
+            }
+        }
+
+
 
 
 

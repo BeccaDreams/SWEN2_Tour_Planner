d3116ee3995eac8fa7306d0c064f37728f0f289a
Fatema Aly <if20b057@technikum-wien.at>
2022-06-29 00:21:27 +0200

    ToolBarView created + UI adaption

---
+++ Tour_Planner/Views/ToolBarView.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tour_Planner.Views
+{
+    /// <summary>
+    /// Interaction logic for ToolBarView.xaml
+    /// </summary>
+    public partial class ToolBarView : UserControl
+    {
+        public ToolBarView()
+        {
+            InitializeComponent();
+        }
+
+        private void export_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void import_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void tourReport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void summarizeReport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FAQ_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+    }
+}

36f5fdf691f13cba0c7b1d5fc479715b4326af41
BeccaDreams <beccadreams94@gmail.com>
2022-06-29 15:45:38 +0200

    DeleteCommandAdded

---
+++ Tour_Planner/Commands/DeleteLogCommand.cs
@@ -0,0 +1,51 @@
+﻿using Shared.Logging;
+using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tour_Planner.ViewModels;
+using Tour_Planner_BL.Controller;
+
+namespace Tour_Planner.Commands
+{
+    public class DeleteLogCommand : BaseCommand
+    {
+
+        private ILoggerWrapper _logger;
+
+        private readonly AddLogToTourViewModel _newLogData;
+        public TourLog _log;
+        LogController _logController;
+        bool deleted;
+
+        public DeleteLogCommand(TourLog _delLog)
+        {
+            _logController = new LogController();
+            _log = _delLog;
+            _logger = LoggerFactory.GetLogger("DeleteLogCommand");
+        }
+
+        public override async void Execute(object parameter)
+        {
+           try
+            {
+                deleted = _logController.Controller_deleteTourLog(_log);
+                if (deleted)
+                {
+                    _logger.Debug("Log was deleted.");
+                }
+                else
+                {
+                    _logger.Debug("Failed deleting Log.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("Exception adding Tour: " + ex.Message);
+            }
+        }
+    }
+}
